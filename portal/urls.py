8fc7fc03a13bd311f8cc610a31ed1ef7c2834e10
Benjamin Tillinger <t-betil@microsoft.com>
2020-06-27 14:20:47 -0700

    Added Initial Views

--- portal/urls.py
+++ portal/urls.py
@@ -5,4 +5,9 @@ from django.conf import settings
 app_name = 'portal'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('casemanager/', views.case_manager_landing_page, name="casemanager"),
+    path('casemanager/student', views.student, name="student"),
+    path('casemanager/student/assignments', views.assignments, name="assignments"),
+    path('casemanager/student/iep', views.iep, name="iep"),
+    path('casemanager/student/discipline', views.discipline, name="discipline"),
 ]
