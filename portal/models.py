474d5edcf48bec79edbc5318d06b1cc2b55ea0e2
Vernon Andrade <t-veand@microsoft.com>
2020-06-27 19:56:58 -0400

    add roster on front page

--- portal/models.py
+++ portal/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Assignment(models.Model):
@@ -6,3 +8,13 @@ class Assignment(models.Model):
     assignment = models.CharField(max_length=200)
     assign_date = models.DateTimeField('date assigned')
     due_date = models.DateTimeField('date published')
+    
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='student')
+    assignments = models.ManyToManyField(Assignment)
+
+
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacher')
+    students = models.ManyToManyField(StudentProfile)
--- portal/views.py
+++ portal/views.py
@@ -3,10 +3,4 @@ from .models import Assignment
 from datetime import timedelta, datetime
 # Create your views here.
 def index(request):
-    assignment = Assignment()
-    assignment.course = "CS 101"
-    assignment.assignment = "500 word essay"
-    assignment.assign_date = datetime.now()
-    assignment.due_date = datetime.now() + timedelta(days=1)
-    assignment.save()
-    return render(request, 'index.html', {'assignment': Assignment.objects.all()})
+    return render(request, 'index.html')
