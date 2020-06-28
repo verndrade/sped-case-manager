e9b4d17ba2470762b266f137dd11342acd094092
Vernon Andrade <t-veand@microsoft.com>
2020-06-27 21:52:20 -0400

    add assignments

--- portal/models.py
+++ portal/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.timezone import make_aware
 
 # Create your models here.
 class Assignment(models.Model):
@@ -8,6 +9,7 @@ class Assignment(models.Model):
     assignment = models.CharField(max_length=200)
     assign_date = models.DateTimeField('date assigned')
     due_date = models.DateTimeField('date published')
+    teacher = models.CharField(max_length=200, default="teacher")
     
 
 class StudentProfile(models.Model):
@@ -18,3 +20,4 @@ class StudentProfile(models.Model):
 class TeacherProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacher')
     students = models.ManyToManyField(StudentProfile)
+    course = models.CharField(max_length=200, default="Algebra")
--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Assignment
 from datetime import timedelta, datetime
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        assign = Assignment()
+        assign.teacher = request.user
+        assign.course = request.user.teacher.course
+        assign.assign_date = datetime.now()
+        if request.POST['duedate']:
+            assign.due_date = request.POST['duedate']
+        else:
+            assign.due_date = assign.assign_date + timedelta(1)
+        assign.save()
+        User.objects.get(username=request.POST['id']).student.assignments.add(assign)
     return render(request, 'index.html')
